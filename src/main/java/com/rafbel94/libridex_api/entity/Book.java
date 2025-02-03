e01d9ce63d3860299480908fc5f8fed97e48c922
Pablorv004 <pablorv004@gmail.com>
2025-02-03 08:30:37 +0100

    Added book entity, updated to 3.4.2

---
+++ src/main/java/com/rafbel94/libridex_api/entity/Book.java
@@ -0,0 +1,123 @@
+package com.rafbel94.libridex_api.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String title;
+
+    @Column
+    private String image;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @Column(nullable = false)
+    private LocalDate publishingDate;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private boolean lent;
+
+    public Book() {
+    }
+
+    public Book(String title, String image, String author, String genre, LocalDate publishingDate, LocalDateTime createdAt,
+            boolean lent) {
+        this.title = title;
+        this.image = image;
+        this.author = author;
+        this.genre = genre;
+        this.publishingDate = publishingDate;
+        this.createdAt = createdAt;
+        this.lent = lent;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getPublishingDate() {
+        return publishingDate;
+    }
+
+    public void setPublishingDate(LocalDate publishingDate) {
+        this.publishingDate = publishingDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isLent() {
+        return lent;
+    }
+
+    public void setLent(boolean lent) {
+        this.lent = lent;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [author=" + author + ", createdAt=" + createdAt + ", genre=" + genre + ", id=" + id + ", image="
+                + image + ", lent=" + lent + ", publishingDate=" + publishingDate + ", title=" + title + "]";
+    }
+}
