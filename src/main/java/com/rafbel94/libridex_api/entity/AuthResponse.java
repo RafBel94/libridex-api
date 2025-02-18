88c81d4dc406c88b3eca2162e5fb9abb354897b5
RafBel94 <beltran.caraf23@cadiz.salesianos.edu>
2025-02-18 19:40:31 +0100

    API update for better compatibility with mobile application

---
+++ src/main/java/com/rafbel94/libridex_api/entity/AuthResponse.java
@@ -0,0 +1,48 @@
+package com.rafbel94.libridex_api.entity;
+
+import java.util.List;
+import java.util.Map;
+
+public class AuthResponse {
+    boolean success;
+    List<String> message;
+    Map<String, Object> data;
+
+    public AuthResponse() {
+    }
+
+    public AuthResponse(boolean success, List<String> message, Map<String, Object> data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public List<String> getMessage() {
+        return message;
+    }
+
+    public void setErrors(List<String> message) {
+        this.message = message;
+    }
+
+    public Map<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, Object> data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiResponse [success=" + success + ", messageg=" + message + ", data=" + data + "]";
+    }
+}
